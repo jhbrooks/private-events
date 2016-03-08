bf0a49b40b7bd9efa6f3a3fc78ef939ef5abfcfe
James Brooks <jhbrooks@uchicago.edu>
2016-03-07 19:22:20 -0500

    Add home page

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  test "site layout" do
+    get root_path
+    assert_select "title", full_title
+    assert_template "static_pages/home"
+    assert_select "a[href=?]", root_path, count: 1
+    assert_select "a[href=?]", "#", text: "Log in", count: 1
+    assert_select "a[href=?]", "#", text: "Sign up now!", count: 1
+  end
+end
