3f97acb246c03df0568bccfa922683b7d1744743
James Brooks <jhbrooks@uchicago.edu>
2016-03-12 17:36:55 -0500

    Properly add foreign key to events (take two)

--- db/migrate/20160311185357_create_events.rb
+++ db/migrate/20160311185357_create_events.rb
@@ -4,9 +4,10 @@ class CreateEvents < ActiveRecord::Migration
       t.string :name
       t.text :description
       t.datetime :event_date
-      t.references :host, index: true, foreign_key: true
+      t.references :host, index: true
 
       t.timestamps null: false
     end
+    add_foreign_key :events, :users, column: :host_id
   end
 end
--- db/migrate/20160312222445_add_foreign_key_to_events.rb
+++
@@ -1,5 +0,0 @@
-class AddForeignKeyToEvents < ActiveRecord::Migration
-  def change
-    add_foreign_key :events, :users, column: :host_id
-  end
-end
